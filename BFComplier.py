1bd08292b8c8f9ecd5d30d6754e7726d7132a98a
William Smith <liamcs98@gmail.com>
2017-02-20 22:09:54 -0500

    Why am I doing this with my life? Also no ,

---
+++ BFComplier.py
@@ -0,0 +1,80 @@
+#This is going to input either a file of BF, or an input
+import sys
+dataPoints = []
+BFProgram = ""
+CurrentPosition = 0
+indexIntoString = 0
+
+def createArrayOfData():
+	for x in range(30000):
+		dataPoints.append(0)
+	#print(dataPoints)
+
+def parseBFProgram():
+	file = open("BFTestProgram.txt", "r")
+	global BFProgram
+	for line in file:
+		for element in line:
+			BFProgram += element
+
+
+
+def meatAndPotatos():
+	global BFProgram
+	global indexIntoString
+	global CurrentPosition
+
+	while indexIntoString < len(BFProgram):
+		element = BFProgram[indexIntoString]
+		if element == "+":
+			dataPoints[CurrentPosition] += 1
+		elif element == "-":
+			dataPoints[CurrentPosition] -= 1
+		elif element == ">":
+			#print(">")
+			CurrentPosition += 1
+		elif element == "<":
+			#print("<")
+			CurrentPosition -= 1
+		elif element == ".":
+			sys.stdout.write(chr(dataPoints[CurrentPosition]))
+			sys.stdout.flush()
+		elif element == ",":
+			pass
+		elif element == "[":
+			if dataPoints[CurrentPosition] == 0:
+				indexIntoString += 1
+				howdeep = 1
+				while howdeep != 0:
+					char = BFProgram[indexIntoString]
+					if char =="[":
+						howdeep += 1
+					elif char == "]":
+						howdeep -= 1
+					indexIntoString += 1
+				indexIntoString -= 1
+
+
+		elif element == "]":
+			if dataPoints[CurrentPosition] != 0:
+				indexIntoString -= 1
+				howdeep = 1
+				while howdeep != 0:
+					char = BFProgram[indexIntoString]
+					if char =="[":
+						howdeep -= 1
+					elif char == "]":
+						howdeep += 1
+					indexIntoString -= 1
+				indexIntoString += 1
+
+		indexIntoString += 1
+
+
+
+def main():
+	createArrayOfData()
+	parseBFProgram()
+	meatAndPotatos()
+
+main()
